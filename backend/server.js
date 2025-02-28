5269bf64e0032e34c80639ba14854be83f04824c
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-02-28 11:32:24 -0500

    fullstack - initial setup w/ signup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+app.use(cors());
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookie parser
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
