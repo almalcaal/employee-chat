47c7bf2c14e70547552ddd774c49f9186324a885
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-02 22:42:05 -0500

    fullstack - signup & login, with correction of backend loginUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from "react-router";
+import Navbar from "./components/common/Navbar.component.jsx";
+import HomeScreen from "./screens/common/Home.screen.jsx";
+import SignUpScreen from "./screens/common/SignUp.screen.jsx";
+import LoginScreen from "./screens/common/Login.screen.jsx";
+import SettingsScreen from "./screens/common/Settings.screen.jsx";
+import ProfileScreen from "./screens/common/Profile.screen.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size=10 animate-spin" />
+      </div>
+    );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomeScreen /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpScreen /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginScreen /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsScreen />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfileScreen /> : <Navigate to="/login" />}
+        />
+      </Routes>
 
-export default App
+      <Toaster position="top-center" reverseOrder={false} />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,71 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (err) {
+      console.log("Error in checkAuth useAuthStore:", err);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (err) {
+      console.log(
+        "Error in signUp useAuthStore:",
+        err?.response?.data?.message || err.error
+      );
+      toast.error("User sign in failed, please try again later");
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (err) {
+      console.log(
+        "Error in login useAuthStore:",
+        err?.response?.data?.message || err.error
+      );
+      toast.error("Failed to log in, please try again later");
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (err) {
+      console.log(
+        "Error in logout useAuthStore:",
+        err?.response?.data?.message || err.error
+      );
+      toast.error("Error logging out, please try again later");
+    }
+  },
+}));
