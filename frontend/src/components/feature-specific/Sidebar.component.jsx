4defd257eb4f74cdd2d54bda9cffc6fc327d8491
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-04 23:04:33 -0500

    frontend - chat container

--- frontend/src/components/feature-specific/Sidebar.component.jsx
+++ frontend/src/components/feature-specific/Sidebar.component.jsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <Users className="size-6" />
-          <span className="font-medium hidden lg-block">Contacts</span>
+          <span className="font-medium lg-block">Contacts</span>
         </div>
         {/* todo: online filter toggle */}
       </div>
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios.js";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -38,6 +38,23 @@ export const useChatStore = create((set) => ({
       toast.error("Fetching messages failed, please try again later");
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (err) {
+      console.log(
+        "Error in sendMessage useChatStore:",
+        err?.response?.data?.message || err.error
+      );
+      toast.error("Sending message failed, please try again later");
     }
   },
 
