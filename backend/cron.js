c534739df1a4f472e32198aa665b879f518b52a6
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-14 00:18:49 -0400

    url switched

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://expense-tracker-jawr.onrender.com/";
+const URL = "https://employee-chat.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
